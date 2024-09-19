1dfa6a24c83d55077fcd1a48db21239f22f47e2a
thinhvn96 <vungocthinhbk@gmail.com>
2024-09-19 15:55:45 +0700

    update

--- table-configs/als_utilization_table_config.js
+++ table-configs/als_utilization_table_config.js
@@ -2,7 +2,7 @@ export const alsUtilizationTableConfig = [
     { name: 'res_customer_id', length: 10 },
     { name: 'res_facility_id', length: 14 },
     { name: 'res_loan_acno', length: 26 },
-    { name: 'res_tran_status', length: 4 },
+    { name: 'res_tran_status', length: 1 },
     { name: 'res_loan_curr', length: 3 },
     { name: 'res_loan_amt', length: 16 },
     { name: 'res_maker_id', length: 10 },

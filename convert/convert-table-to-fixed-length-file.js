afdf7d9a451e0b3b965cdded2dfd89e3e8488717
thinhvn96 <vungocthinhbk@gmail.com>
2024-09-25 10:40:10 +0700

    update

--- convert/convert-table-to-fixed-length-file.js
+++ convert/convert-table-to-fixed-length-file.js
@@ -23,8 +23,14 @@ export function convertTableToFixedLength() {
     for (let i = 1; i < rows.length; i++) { // Start from 1 to skip the header row
         const inputs = rows[i].getElementsByTagName('input');
         for (let j = 0; j < inputs.length; j++) {
-            if(inputs[j].value && inputs[j].value.trim())
+
+            if(columns[j].source === 'TI') {
+                if(inputs[j].value && inputs[j].value.trim())
+                    fixedLengthText += padString(inputs[j].value, columns[j].length);
+            } else {
                 fixedLengthText += padString(inputs[j].value, columns[j].length);
+            }
+
             if(j <= inputs.length - 2)
                 fixedLengthText += separator;
         }
